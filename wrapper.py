413b87a633ac51a907f69322dc3fb3ce85f19d77
pbiology <anders.lind.cgggu.se>
2022-07-12 15:24:03 +0200

    Read in config and start loggin. Also som better inline comments for sections

--- wrapper.py
+++ wrapper.py
@@ -1,23 +1,49 @@
+import os
+import datetime
 import rich_click as click
-from tools.slims import SlimsSample, translate_slims_info, samples_from_sec_analysis, fastq_paths
+
+from tools.helpers import (
+    setup_logger,
+    get_config,)
+from tools.slims import (
+    SlimsSample,
+    translate_slims_info,
+    samples_from_sec_analysis,
+    fastq_paths,)
 
 @click.command()
 @click.option(
-    "--log",
-    help="Path to wrapper log file. Default value set in config.ini",
+    "--logdir",
+    help="Path to directory of wrapper log file. Default value set in config.ini",
 )
-def main():
-    # Find all slims records marked for QD-RNAseq pipeline as secondary analysis
+def main(logdir):
+    ### --- Read in the config --- ###
+    config = get_config()
+
+    #### ------- Set up the logger function --- ###
+    now = datetime.datetime.now()
+    if not logdir:
+        logdir = config.get("general", "wrapper_log_dir")
+
+    os.makedirs(logdir, exist_ok=True)
+
+    logfile = os.path.join(
+        logdir,
+        "QD-rnaseq-wrapper_" + now.strftime("%y%m%d_%H%M%S") + ".log",
+        )
+    logger = setup_logger("qd-rnaseq", logfile)
+    logger.info("Starting the RNAseq pipepline wrapper.")
+
+    ### --- Find all slims records marked for QD-RNAseq pipeline as secondary analysis --- ###
     rnaseq_samples = samples_from_sec_analysis(186)
     # 29 = WOPR
     # 186 = QD-RNA
 
-    # Loop over each record and gather relevant information
-    # Start the runner in a separate thread for each sample
+    ### --- Loop over each record --- ###
+    # Gather relevant information for each sample
     for sample, record in rnaseq_samples.items():
         # Create a new SlimsSample object
         slimsinfo = SlimsSample(sample)
-
 
         # Save fastq paths for forward and reverse in separate lists
         fastqs = fastq_paths(slimsinfo.fastqs)
